0816c5a05f496a687c8485877a2f1e877378e96a
VictorLM <victordinami@gmail.com>
2018-06-18 09:52:18 -0300

    Revert "Alteração texto home"
    This reverts commit 1491a679fdc8f62f78e20cfd2404bc776855f409.

--- resources/views/intranet/index.blade.php
+++ resources/views/intranet/index.blade.php
@@ -4,21 +4,21 @@
 @endpush
 @section('content')
 
-<div class="container-index">
-
-    @foreach (['danger', 'warning', 'success', 'info'] as $msg)
-        @if(Session::has('alert-' . $msg))
-            <div class="alert alert-success alert-dismissable" style="margin-left:10px;margin-right:10px;">
-                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                {{ Session::get('alert-' . $msg) }}
-            </div>
-        @endif
-    @endforeach
-
-    <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 cols-index">
-
-        <div class="panel panel-default">
-            <div class="panel-heading">Menu</div>
+<div class="container">
+    
+        <div class="col-md12">
+            
+            @foreach (['danger', 'warning', 'success', 'info'] as $msg)
+                @if(Session::has('alert-' . $msg))
+                    <div class="alert alert-success alert-dismissable">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        {{ Session::get('alert-' . $msg) }}
+                    </div>
+                @endif
+            @endforeach
+            
+            <div class="panel panel-default">
+                <div class="panel-heading">Menu</div>
 
             <div class="panel-body">
                 @if (session('status'))
@@ -148,94 +148,60 @@
                     
                 </div>
                 
-            </div>
-        </div>
-    </div>
-
-    <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 cols-index">
-        <div class="panel panel-default">
-            <div class="panel-heading text-center">
-                <h2 class="uau-ranking-index" style="font-family: 'Dosis', sans-serif;">
-                    <a class="uau-ranking-index" href="{{url('intranet/uau')}}">UAU! - Ranking</a>
-                </h2>
-            </div>
-            <div class="panel-body">
-                @if(!$ranking->isEmpty())
-                    @php
-                        $max = $ranking[0]->uaus;
-                    @endphp
-                @endif 
-                @foreach($ranking as $user)
-                    <small>
-                        <strong>
-                            @if($user->id == Auth::user()->id) 
-                                <i class="glyphicon glyphicon-heart" style="color:red;"></i>
-                            @endif 
-                            {{$user->name}}
-                        </strong>
-                    </small>
-                    <div class="progress">
-                        <div class="progress-bar" role="progressbar" aria-valuenow="{{$user->uaus}}"
-                        aria-valuemin="0" aria-valuemax="{{$max}}" style="width:{{$user->uaus/$max*100}}%">
-                            {{$user->uaus}} @if($user->uaus>1) Uaus! @else Uau! @endif 
+                @if ($aniversario)
+                    <!-- Modal -->
+                    <div class="modal fade" id="parabensModal" tabindex="-1" role="dialog" aria-labelledby="memberModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content aniversario">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h1 class="modal-title" id="memberModalLabel">Parabéns!</h1>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Parabéns pelo seu aniversário! Muita saúde e felicidade.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                @endforeach
+                    @push ('scripts')
+                        <script type="text/javascript">
+                            $('#parabensModal').modal('show');
+                        </script>
+                    @endpush
+                @endif
+                
+                @if ($unread_uaus>0)
+                    <!-- Modal -->
+                    <div class="modal fade" id="uauModal" tabindex="-1" role="dialog" aria-labelledby="memberModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content modaluau">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <h1 class="modal-title text-center uau-title">UAU!</h1>
+                                </div>
+                                <div class="modal-body">
+                                    <h2 class="modal-title" id="memberModalLabel">Parabéns {{Auth::user()->name}}, você recebeu um novo Uau!</h2>
+                                    <h3>Clique <a href="{{url("/intranet/meus-uaus")}}">aqui</a> para visualizar.</h3>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @push ('scripts')
+                        <script type="text/javascript">
+                            $('#uauModal').modal('show');
+                        </script>
+                    @endpush
+                @endif
+
             </div>
         </div>
     </div>
-
-    @if ($aniversario)
-        <!-- Modal -->
-        <div class="modal fade" id="parabensModal" tabindex="-1" role="dialog" aria-labelledby="memberModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content aniversario">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h1 class="modal-title" id="memberModalLabel">Parabéns!</h1>
-                    </div>
-                    <div class="modal-body">
-                        <p>Parabéns pelo seu aniversário! Muita saúde e felicidade.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @push ('scripts')
-            <script type="text/javascript">
-                $('#parabensModal').modal('show');
-            </script>
-        @endpush
-    @endif
-    
-    @if ($unread_uaus>0)
-        <!-- Modal -->
-        <div class="modal fade" id="uauModal" tabindex="-1" role="dialog" aria-labelledby="memberModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content modaluau">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h1 class="modal-title text-center uau-title">UAU!</h1>
-                    </div>
-                    <div class="modal-body">
-                        <h2 class="modal-title" id="memberModalLabel">Parabéns {{Auth::user()->name}}, você recebeu um novo Uau!</h2>
-                        <h3>Clique <a href="{{url("/intranet/meus-uaus")}}">aqui</a> para visualizar.</h3>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Fechar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @push ('scripts')
-            <script type="text/javascript">
-                $('#uauModal').modal('show');
-            </script>
-        @endpush
-    @endif
-
 </div>
 @push ('scripts')
     <script src="{{ asset('assets/js/app.js') }}"></script>
